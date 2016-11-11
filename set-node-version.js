64198b9731cfaa3467565ca005e28f034f17a05a
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-11-11 15:05:21 -0500

    Set node version to >= 6.

--- set-node-version.js
+++ set-node-version.js
@@ -2,11 +2,11 @@
 
 const semver = require('semver');
 const {
-    requiredNodeVersion
+    node
 } = require('./package.json').engines;
 
 module.exports = function () {
-    const isOkNodeVersion = semver.satisfies(process.versions.node, requiredNodeVersion);
+    const isOkNodeVersion = semver.satisfies(process.versions.node, node);
     if (!isOkNodeVersion) {
         console.error(
             `redux adventures needs Node.js version ${requiredNodeVersion}, but found to be running Node.js ${process.version}`
